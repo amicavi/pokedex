ea07afebc9247dd90ef02786fc4ad77d1fb1c3e1
amicavi <dev.patsy.iv@gmail.com>
2019-01-06 15:26:47 -0600

    changed, move functionality to get pokemon detail from App.js to DetailView component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './img/logo-pokemon.png';
 import './App.css';
-import Handlers from './utilities/handlers.js';
+// import Handlers from './utilities/handlers.js';
 
 import { SearchBar } from './components/SearchBar/index.jsx';
 import DetailView from './components/DetailView/index.jsx';
@@ -9,20 +9,13 @@ import PokemonList from './components/PokemonList/index.jsx';
 
 class App extends Component {
   state = {
-    profile : {},
-    isEmptyState : true,
-  }
-
-  updateProfile = (profile) => {
-    this.setState({
-      profile: profile,
-      isEmptyState : false
-    })
+    selected_pokemon_id : "",
   }
 
   pokemonSelectionHandle = (id) => {
-    console.log(id)
-    Handlers.getPokemonDetails(id, this.updateProfile)
+    this.setState({
+      selected_pokemon_id: id,
+    })
   }
 
   render() {
@@ -32,7 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="Pokemon logo" />
         </header>
         <main className="col-xs-12 col-lg-10 col-lg-offset-1">
-          <DetailView profile={this.state.profile} isEmptyState={this.state.isEmptyState}/>
+          <DetailView id={this.state.selected_pokemon_id}/>
           <SearchBar />
           <PokemonList
             selection_handler = {this.pokemonSelectionHandle}
