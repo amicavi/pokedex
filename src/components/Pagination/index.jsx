a6fd7db49d71aa9dfcf6bc029bf25bad2a687be3
amicavi <dev.patsy.iv@gmail.com>
2019-01-06 14:49:33 -0600

    fixed, pagination next and previous bug after changing ofset for page information

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -3,17 +3,17 @@ import './styles.css';
 
 class Pagination extends Component {
   
-  getPages = (count, limit, current_page) => {
+  getPages = (count, limit, current_page, current_offset) => {
     const total_pages = Math.ceil(count / limit);
     const page_limit = current_page > 2 ? current_page + 2 : 4;
     let page_number = current_page > 2 ? current_page - 2 : 0;
-    let offset = current_page > 0 ? (page_number * 50) : 0;
+    let offset = current_offset? current_offset : 0;
     let pages = [];
 
     while (page_number <= page_limit && page_limit <= total_pages) {
       pages.push({
         offset: offset,
-        page_number: page_number,
+        page_number: page_number + 1,
         active: page_number === current_page ? "active" : ""
       })
       page_number ++;
@@ -24,19 +24,20 @@ class Pagination extends Component {
 
   render() {
     const { previous, next, current_page, count, limit, onPaginationClick} = this.props;
+    const current_offset = current_page * 50
 
     return (
       <nav aria-label="Page navigation">
         <ul className="pagination">
           {previous != null && (
             <li>
-              <button className="page" aria-label="Previous" onClick={()=> onPaginationClick(current_page - limit)}>
+              <button className="page" aria-label="Previous" onClick={()=> onPaginationClick(current_offset - limit)}>
                 <span aria-hidden="true">&laquo;</span>
               </button>
             </li>
           )}
           { 
-            this.getPages(count, limit, current_page).map(page => (
+            this.getPages(count, limit, current_page, current_offset).map(page => (
               <li key={page.page_number}>
                 <button className={"page " + page.active} onClick={()=> onPaginationClick(page.offset)}>
                   {page.page_number}
@@ -46,7 +47,7 @@ class Pagination extends Component {
           }
           {next != null && (
             <li>
-              <button className="page" aria-label="Next" onClick={()=> onPaginationClick(current_page + limit)}>
+              <button className="page" aria-label="Next" onClick={()=> onPaginationClick(current_offset + limit)}>
                 <span aria-hidden="true">&raquo;</span>
               </button>
             </li>
