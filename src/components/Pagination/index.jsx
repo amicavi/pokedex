2f752a47036d8262ee56cd91b292b13330658fd2
amicavi <dev.patsy.iv@gmail.com>
2019-01-06 22:50:38 -0600

    changed, set offset of pages given its page number and avoid magic numbers

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import './styles.css';
+import constants from '../../utilities/constants.js';
+
 
 class Pagination extends Component {
 
@@ -7,7 +9,7 @@ class Pagination extends Component {
     const total_pages = Math.ceil(count / limit);
     const page_limit = current_page > 2 ? current_page + 2 : 4;
     let page_number = current_page > 2 ? current_page - 2 : 0;
-    let offset = current_offset? current_offset : 0;
+    let offset = page_number * constants.pagination.limit;
     let pages = [];
 
     while (page_number <= page_limit && page_limit <= total_pages) {
@@ -24,12 +26,12 @@ class Pagination extends Component {
 
   render() {
     const { previous, next, current_page, count, limit, onPaginationClick} = this.props;
-    const current_offset = current_page * 50
+    const current_offset = current_page * constants.pagination.limit;
 
     return (
       <nav aria-label="Page navigation">
         <ul className="pagination">
-          {previous && (
+          {previous !== null && (
             <li>
               <button className="page" aria-label="Previous" onClick={()=> onPaginationClick(current_offset - limit)}>
                 <span aria-hidden="true">&laquo;</span>
@@ -39,7 +41,7 @@ class Pagination extends Component {
           {
             this.getPages(count, limit, current_page, current_offset).map(page => (
               <li key={page.page_number}>
-                <button className={"page " + page.active} onClick={()=> onPaginationClick(page.offset)}>
+                <button className={"page " + page.active} offset={page.offset} onClick={()=> onPaginationClick(page.offset)}>
                   {page.page_number}
                 </button>
               </li>
